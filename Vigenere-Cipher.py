1c30fe309300fc9afa35c6269652bf1dde52d760
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 19:25:35 +0800

    Asked for user's message and keyword input.

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -12,10 +12,16 @@ letter_to_index = dict(zip(letter_list, range(len(letter_list))))
 # Make a dictionary for index range to letter.
 index_to_letter = dict(zip(range(len(letter_list)), letter_list))
 
-print (letter_to_index)
-print (index_to_letter)
 
 # Ask the user for message and keyword.
+def main():
+    user_message = input("Please input your message here: ").upper()
+    user_keyword = input("Please input your keyword here: ").upper()
+
 
 # Decryption/encryption of the message with keyword.
+
+
 # Print the ciphered message.
+    
+main ()
