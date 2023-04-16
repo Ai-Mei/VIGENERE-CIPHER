05d1006475777df8f251ae5e96ce5c9025f07230
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 19:11:14 +0800

    The dictionaries are working properly.

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -1,9 +1,21 @@
 # VIGENÈRE CIPHER
 
 # Import string function to generate list.
-# Make a list for ASCII.
-# Make a dictionary for letter to range.
-# Make a dictionary for range to letter.
+import string
+
+# Make a list for upper case ASCII letters.
+letter_list = list(string.ascii_uppercase)
+
+# Make a dictionary for letter to index range.
+letter_to_index = dict(zip(letter_list, range(len(letter_list))))
+
+# Make a dictionary for index range to letter.
+index_to_letter = dict(zip(range(len(letter_list)), letter_list))
+
+print (letter_to_index)
+print (index_to_letter)
+
 # Ask the user for message and keyword.
+
 # Decryption/encryption of the message with keyword.
 # Print the ciphered message.
