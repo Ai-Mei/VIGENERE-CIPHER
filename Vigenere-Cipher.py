9204d2d337a3f06686a34dd5f1a877ade27c3f26
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 19:06:40 +0800

    Psuedocode

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -1,1 +1,9 @@
+# VIGENÈRE CIPHER
 
+# Import string function to generate list.
+# Make a list for ASCII.
+# Make a dictionary for letter to range.
+# Make a dictionary for range to letter.
+# Ask the user for message and keyword.
+# Decryption/encryption of the message with keyword.
+# Print the ciphered message.
